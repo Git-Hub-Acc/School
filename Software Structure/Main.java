7f0c62b3d737cd90b645f3050eaa344786478853
Git-Hub-Acc <31142483+Git-Hub-Acc@users.noreply.github.com>
2017-08-26 12:39:30 +0300

    Add files via upload

--- Software Structure/Main.java
+++ Software Structure/Main.java
@@ -1,36 +1,28 @@
 package ex1;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        Hash_Pwds_JSON hpj = new Hash_Pwds_JSON();
+        Hash_Pwds_JSON hpj = new Hash_Pwds_JSON(); // Create Hash_Pwds_JSON object.
 
         int num = 10;
-        long[] durations = checkDuration(hpj, num);
+        long[] durations = checkDuration(hpj, num); // Execute checkDuration() function 10 times.
 
         printArr(durations);
 
-        long memUsed = checkMemUsage(hpj);
-        System.out.println("Memory used: " + memUsed);
+        long memUsed = checkMemUsage(hpj); // how much memory the program used.
+        System.out.println("Memory used: " + memUsed + " Mbs.");
 
     }
 
     /* -------------------------------------------------
-		Check time at the start of the program.
-		Parameters:
-			hpj - Hash_Pwds_JSON object to test.
-			num - Number of times to check.
-		Return:
-			An array of run durations.
+    Check time at the start of the program.
+        Parameters:
+            hpj - Hash_Pwds_JSON object to test.
+            num - Number of times to check.
+        Return:
+            An array of run durations.
      -------------------------------------------------*/
     public static long[] checkDuration(Hash_Pwds_JSON hpj, int num) {
         long[] durations = new long[num];
@@ -43,52 +35,43 @@ public class Main {
             // Check time at the end of the program.
             long endTime = System.currentTimeMillis();
 
-            //System.out.println("starTime: " + startTime); //DEBUG
-            //System.out.println("endTime: " + endTime); //DEBUG
-
             // Total running time of the program.
             long duration = endTime - startTime;
-            //System.out.println("Duration of the program: " + duration); //DEBUG
 
             durations[i] = duration;
         }
-
         return durations;
     }
 
     /* -------------------------------------------------
-		Calculates the memory used by the program.
-		Parameters:
-			hpj - Hash_Pwds_JSON object to test.
-		Return:
-			The amount of memory used.
+    Calculates the memory used by the program.
+        Parameters:
+            hpj - Hash_Pwds_JSON object to test.
+        Return:
+            The amount of memory used.
      -------------------------------------------------*/
     public static long checkMemUsage(Hash_Pwds_JSON hpj) {
 
         System.gc(); // Call garbage collector to get more accurate results.
         Runtime runTime = Runtime.getRuntime();
-        long memBefore = runTime.totalMemory() - runTime.freeMemory();
+        long memBefore = runTime.totalMemory() - runTime.freeMemory(); // Memory used before the program execution.
 
         hpj.hash();
 
-        // Check memory after the program runs.
+        // Check memory after the program's execution.
         long memAfter = runTime.totalMemory() - runTime.freeMemory();
 
         // Calculate memory usage.
         long memUsed = memAfter - memBefore;
 
-        //System.out.println("memBefore: " + memBefore); //DEBUG
-        //System.out.println("memAfter: " + memAfter); //DEBUG
-        //System.out.println("Memory used: " + memUsed); //DEBUG
-        
         return memUsed;
     }
-    
+
     // Function to print an array.
     public static void printArr(long[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
+        for (int i = 0; i < arr.length - 1; i++)
             System.out.print(arr[i] + ", ");
-        }
+        
         System.out.println(arr[arr.length - 1]);
     }
 }
